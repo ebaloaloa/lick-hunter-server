bc2d7671cd0ab40b7a8c2542976360ad76f47002
Ephraim Baloaloa <ebaloaloa@gmail.com>
2021-03-20 21:38:43 +0800

    Externalize properties. Add maven assembly for distribution

---
+++ src/main/java/com/lickhunter/web/properties/ApplicationConfig.java
@@ -0,0 +1,14 @@
+package com.lickhunter.web.properties;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
+
+@Configuration
+@PropertySources({
+        @PropertySource(value = "classpath:application.properties", ignoreResourceNotFound = true),
+        @PropertySource(value = "file:application.properties", ignoreResourceNotFound = true)
+})
+public class ApplicationConfig {
+    private String serverPort;
+}
