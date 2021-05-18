00080a19fd2eba9e34bcd1854b187acf9b6c3e47
Ephraim Baloaloa <ebaloaloa@gmail.com>
2021-05-19 01:52:49 +0800

    Added properties for scheduler income history

--- server/src/main/java/com/lickhunter/web/entities/public_/tables/records/IncomeHistoryRecord.java
+++ server/src/main/java/com/lickhunter/web/entities/public_/tables/records/IncomeHistoryRecord.java
@@ -5,14 +5,16 @@ package com.lickhunter.web.entities.public_.tables.records;
 
 
 import com.lickhunter.web.entities.public_.tables.IncomeHistory;
+
+import java.math.BigDecimal;
+
+import javax.annotation.Generated;
+
 import org.jooq.Field;
 import org.jooq.Record1;
 import org.jooq.Record8;
 import org.jooq.Row8;
 import org.jooq.impl.UpdatableRecordImpl;
-
-import javax.annotation.Generated;
-import java.math.BigDecimal;
 
 
 /**
