f8f57510ed4152012f237d20fb1b39c5a6f46c18
Ephraim Baloaloa <ebaloaloa@gmail.com>
2021-10-31 01:10:30 +0800

    Upgrade okhttp3 version

--- server/src/main/java/com/lickhunter/web/websockets/BinanceSubscription.java
+++ server/src/main/java/com/lickhunter/web/websockets/BinanceSubscription.java
@@ -6,7 +6,6 @@ import com.binance.client.SyncRequestClient;
 import com.binance.client.model.enums.*;
 import com.binance.client.model.market.Candlestick;
 import com.binance.client.model.market.MarkPrice;
-import com.jcabi.aspects.RetryOnFailure;
 import com.lickhunter.web.configs.Settings;
 import com.lickhunter.web.configs.UserDefinedSettings;
 import com.lickhunter.web.configs.WebSettings;
@@ -23,13 +22,11 @@ import com.lickhunter.web.repositories.SymbolRepository;
 import com.lickhunter.web.scheduler.LickHunterScheduledTasks;
 import com.lickhunter.web.services.*;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationEventPublisher;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
 import org.ta4j.core.BarSeries;
@@ -77,44 +74,39 @@ public class BinanceSubscription {
     @Value("${telegram.notification.liquidation}")
     private Boolean tgNotifLiquidation;
 
-
-    @Async
-    @RetryOnFailure(attempts = 5, delay = 5, unit = TimeUnit.SECONDS)
     public void subscribeCandleStickData() {
         log.info("Subscribing to Binance candlesticks data");
-        SubscriptionOptions subscriptionOptions = new SubscriptionOptions();
-        subscriptionOptions.setUri("wss://fstream.binance.com");
-        subscriptionOptions.setConnectionDelayOnFailure(10);
-        SubscriptionClient subscriptionClient = SubscriptionClient.create(subscriptionOptions);
         List<String> symbols = symbolRepository.findAll().stream()
                 .map(SymbolRecord::getSymbol)
                 .filter(s -> s.matches("^.*USDT$"))
                 .map(String::toLowerCase)
                 .collect(Collectors.toList());
-        Arrays.stream(candlesticks).forEach(c -> subscriptionClient.subscribeCandlestickEvent(symbols, CandlestickInterval.of(c), ((event) -> {
-            Candlestick candlestick = new Candlestick();
-            candlestick.setOpenTime(event.getStartTime());
-            candlestick.setOpen(event.getOpen());
-            candlestick.setHigh(event.getHigh());
-            candlestick.setLow(event.getClose());
-            candlestick.setClose(event.getClose());
-            candlestick.setVolume(event.getVolume());
-            candlestick.setCloseTime(event.getCloseTime());
-            candlestick.setQuoteAssetVolume(event.getQuoteAssetVolume());
-            candlestick.setNumTrades(event.getNumTrades().intValue());
-            candlestick.setTakerBuyBaseAssetVolume(event.getTakerBuyBaseAssetVolume());
-            candlestick.setTakerBuyQuoteAssetVolume(event.getTakerBuyQuoteAssetVolume());
-            candlestickRepository.insertOrUpdate(event.getSymbol(), candlestick, CandlestickInterval.of(c));
-        }), e -> {
-            String message = String.format("Error during candlestick subscription event: %s", e.getMessage());
-            telegramService.send(message);
-            log.error(message);
-        }));
+        SubscriptionClient subscriptionClient = SubscriptionClient.create();
+        Arrays.stream(candlesticks)
+            .forEach(c -> {
+                subscriptionClient.subscribeCandlestickEvent(symbols, CandlestickInterval.of(c), ((event) -> {
+                        Candlestick candlestick = new Candlestick();
+                        candlestick.setOpenTime(event.getStartTime());
+                        candlestick.setOpen(event.getOpen());
+                        candlestick.setHigh(event.getHigh());
+                        candlestick.setLow(event.getClose());
+                        candlestick.setClose(event.getClose());
+                        candlestick.setVolume(event.getVolume());
+                        candlestick.setCloseTime(event.getCloseTime());
+                        candlestick.setQuoteAssetVolume(event.getQuoteAssetVolume());
+                        candlestick.setNumTrades(event.getNumTrades().intValue());
+                        candlestick.setTakerBuyBaseAssetVolume(event.getTakerBuyBaseAssetVolume());
+                        candlestick.setTakerBuyQuoteAssetVolume(event.getTakerBuyQuoteAssetVolume());
+                        candlestickRepository.insertOrUpdate(event.getSymbol(), candlestick, CandlestickInterval.of(c));
+                    }), e -> {
+                        String message = String.format("Error during candlestick subscription event: %s", e.getMessage());
+                        telegramService.send(message);
+                        log.error(message);
+                    });
+            });
         log.info("Subscribed to Binance Candlestick data");
     }
 
-    @Async
-    @SneakyThrows
     public void subscribeUserData() {
         log.info("Subscribing to User Data");
         ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
@@ -123,11 +115,7 @@ public class BinanceSubscription {
         String listenKey = syncRequestClient.startUserDataStream();
         Thread listenKeyKeepALive = new Thread(() -> syncRequestClient.keepUserDataStream(listenKey));
         executorService.scheduleWithFixedDelay(listenKeyKeepALive, 0, 45, TimeUnit.MINUTES);
-        SubscriptionOptions subscriptionOptions = new SubscriptionOptions();
-        subscriptionOptions.setReceiveLimitMs(1800000);
-        subscriptionOptions.setUri("wss://fstream.binance.com/");
-        subscriptionOptions.setConnectionDelayOnFailure(3);
-        SubscriptionClient subscriptionClient = SubscriptionClient.create(subscriptionOptions);
+        SubscriptionClient subscriptionClient = SubscriptionClient.create();
         subscriptionClient.subscribeUserDataEvent(listenKey, ((event) -> {
             switch (UserDataEventConstants.valueOf(event.getEventType())) {
                 case ACCOUNT_UPDATE:
@@ -186,44 +174,31 @@ public class BinanceSubscription {
         log.info("Successfully subscribed to User Data.");
     }
 
-    @Async
-    @RetryOnFailure(attempts = 5, delay = 5, unit = TimeUnit.SECONDS)
     public void subscribeMarkPrice() {
         log.info("Subscribing to Binance Mark Price data.");
-        SubscriptionOptions subscriptionOptions = new SubscriptionOptions();
-        subscriptionOptions.setUri("wss://fstream.binance.com/");
-        subscriptionOptions.setConnectionDelayOnFailure(3);
-        SubscriptionClient subscriptionClient = SubscriptionClient.create(subscriptionOptions);
-        try {
-            subscriptionClient.subscribeAllMarkPriceEvent(data -> data.forEach(event -> {
-                if(event.getEventType().contains("markPriceUpdate")) {
-                        MarkPrice markPrice = new MarkPrice();
-                        markPrice.setMarkPrice(event.getMarkPrice());
-                        markPrice.setSymbol(event.getSymbol());
-                        markPrice.setTime(event.getEventTime());
-                        markPrice.setLastFundingRate(event.getFundingRate());
-                        markPrice.setNextFundingTime(event.getNextFundingTime());
-                        symbolRepository.insertOrUpdate(markPrice);
-                    }
-            }), e -> {
-                String message = String.format("Error during mark price subscription event: %s.", e.getMessage());
-                telegramService.send(message);
-                log.error(message);
-            });
-        } catch (Exception e) {
-            subscriptionClient.unsubscribeAll();
-            log.error("Unsubscribed to Mark Price Events.");
-        }
+        SubscriptionClient subscriptionClient = SubscriptionClient.create();
+        subscriptionClient.subscribeAllMarkPriceEvent(data -> data.forEach(event -> {
+            if(event.getEventType().contains("markPriceUpdate")) {
+                MarkPrice markPrice = new MarkPrice();
+                markPrice.setMarkPrice(event.getMarkPrice());
+                markPrice.setSymbol(event.getSymbol());
+                markPrice.setTime(event.getEventTime());
+                markPrice.setLastFundingRate(event.getFundingRate());
+                markPrice.setNextFundingTime(event.getNextFundingTime());
+                symbolRepository.insertOrUpdate(markPrice);
+            }
+        }), e -> {
+            String message = String.format("Error during mark price subscription event: %s.", e.getMessage());
+            telegramService.send(message);
+            log.error(message);
+        });
     }
 
-     @SneakyThrows
      public void subscribeLiquidation() {
-         SubscriptionOptions subscriptionOptions = new SubscriptionOptions();
-         subscriptionOptions.setUri("wss://fstream.binance.com/");
-         subscriptionOptions.setConnectionDelayOnFailure(3);
          Settings settings = lickHunterService.getLickHunterSettings();
-         SubscriptionClient subscriptionClient = SubscriptionClient.create(subscriptionOptions);
+         SubscriptionClient subscriptionClient = SubscriptionClient.create();
          subscriptionClient.subscribeAllLiquidationOrderEvent(data -> {
+             log.debug(data.toString());
              List<SymbolRecord> symbolRecords = symbolRepository.findTradeableSymbols();
              symbolRecords.stream()
                      .filter(c -> c.getSymbol().equalsIgnoreCase(data.getSymbol()))
@@ -232,12 +207,12 @@ public class BinanceSubscription {
                          UserDefinedSettings activeSettings = lickHunterService.getActiveSettings();
                          Boolean isMedian = Objects.nonNull(activeSettings.getAutoLickValue())
                                  ? lickHunterService.getActiveSettings().getAutoLickValue().equalsIgnoreCase("median")
-                                    : null;
+                                 : null;
                          BigDecimal lickValue = Objects.isNull(isMedian) ?
-                                                 BigDecimal.valueOf(activeSettings.getLickValue()) :
-                                                 isMedian ?
-                                                         BigDecimal.valueOf(c.getLickMedian()) :
-                                                         BigDecimal.valueOf(c.getLickAverage());
+                                 BigDecimal.valueOf(activeSettings.getLickValue()) :
+                                 isMedian ?
+                                         BigDecimal.valueOf(c.getLickMedian()) :
+                                         BigDecimal.valueOf(c.getLickAverage());
                          BarSeries barSeries = technicalIndicatorService.getBarSeries(data.getSymbol(), CandlestickInterval.of(lickHunterService.getWebSettings().getVwapTimeframe()));
                          Optional<SymbolRecord> symbolRecord = symbolRepository.findBySymbol(data.getSymbol());
                          BigDecimal qty = this.getQty(symbolRecord.get());
